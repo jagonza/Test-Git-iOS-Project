379759e8f7a62a85b09f13038453675bd7907936
Javier González Rojo <Javier@MacBook-Air-de-Javier.local>
2014-03-28 19:07:53 +0100

    added a Test class

---
+++ Git Test/GTTest.h
@@ -0,0 +1,13 @@
+//
+//  GTTest.h
+//  Git Test
+//
+//  Created by Javier González Rojo on 28/03/14.
+//  Copyright (c) 2014 Javier González Rojo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTTest : NSObject
+
+@end
---
+++ Git Test/GTTest.m
@@ -0,0 +1,13 @@
+//
+//  GTTest.m
+//  Git Test
+//
+//  Created by Javier González Rojo on 28/03/14.
+//  Copyright (c) 2014 Javier González Rojo. All rights reserved.
+//
+
+#import "GTTest.h"
+
+@implementation GTTest
+
+@end
